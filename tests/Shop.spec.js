0fad5981cf6fb00e9ce68216c55bbe8578762483
shanedaniel484 <shanedaniel484@gmail.com>
2025-03-19 23:29:28 +0530

    completed till clicking checkout

--- tests/Shop.spec.js
+++ tests/Shop.spec.js
@@ -1,6 +1,44 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Place Order' , async({browser})=>
-{
+test.only('Place Order', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    // elements
+    const emailId = page.locator("#userEmail");
+    const password = page.locator("#userPassword");
+    const loginBtn = page.locator("#login");
+    const products = page.locator(".card-body");
+    const productName = page.locator(".card-body b");
+    const productToBeAdded = "ADIDAS ORIGINAL";
+    const cartBtn = page.locator("[routerlink='/dashboard/cart']");
+    const checkoutBtn = page.locator("button:has-text('Checkout')");
+
+    // actions
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await emailId.fill("playwrightomm@gmail.com");
+    await password.fill("Test@123");
+    await loginBtn.click();
+    await expect(page).toHaveTitle("Let's Shop");
+    await page.waitForLoadState('networkidle');
+    const count = await products.count();
+    const productList = await productName.allTextContents();
+    console.log("The number of products is " + count, "The products are - " + productList);
     
-})
+    for (let i = 0; i < count; ++i) {
+        if (await products.nth(i).locator("b").textContent() === productToBeAdded) {
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
+    
+    await cartBtn.click();
+    await page.locator("div li").first().waitFor();
+    const bool = await page.locator(`h3:has-text('${productToBeAdded}')`).isVisible();
+    expect(bool).toBeTruthy();
+    
+    await checkoutBtn.click();
+    await page.locator(".item__title").first().waitFor();
+    const bool2 = await page.locator(`div:has-text('${productToBeAdded}')`).first().isVisible();
+    expect(bool2).toBeTruthy();
+});
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,112 +1,112 @@
-const {test, expect} = require('@playwright/test');
-//const { text } = require('stream/consumers');
+// const {test, expect} = require('@playwright/test');
+// //const { text } = require('stream/consumers');
 
-test('Browser', async ({page})=>
-{
-    // const context = await browser.newContext();
-    // const page = await context.newPage();
-    //elements
-    const UserName = page.locator("#username");
-    const Password = page.locator("[id='password']");
-    const CheckBox = page.locator("[type='checkbox']");
-    const SignInBtn = page.locator("#signInBtn");
-    const CardTitles = page.locator(".card-body a");
+// test('Browser', async ({page})=>
+// {
+//     // const context = await browser.newContext();
+//     // const page = await context.newPage();
+//     //elements
+//     const UserName = page.locator("#username");
+//     const Password = page.locator("[id='password']");
+//     const CheckBox = page.locator("[type='checkbox']");
+//     const SignInBtn = page.locator("#signInBtn");
+//     const CardTitles = page.locator(".card-body a");
 
-    //actions
-    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-    console.log(await page.title());
-    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-    await UserName.fill("rahulshettyacademy");
-    //await page.locator("#username").fill("rahulsmy");
-    await Password.fill("learning");
-    await CheckBox.click();
-    await SignInBtn.click();
-    //success condition
-    await expect(page).toHaveTitle("ProtoCommerce");
-    //fail condition
-    // console.log(await page.locator("[style*='block']").textContent());
-    // await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+//     //actions
+//     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+//     console.log(await page.title());
+//     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+//     await UserName.fill("rahulshettyacademy");
+//     //await page.locator("#username").fill("rahulsmy");
+//     await Password.fill("learning");
+//     await CheckBox.click();
+//     await SignInBtn.click();
+//     //success condition
+//     await expect(page).toHaveTitle("ProtoCommerce");
+//     //fail condition
+//     // console.log(await page.locator("[style*='block']").textContent());
+//     // await expect(page.locator("[style*='block']")).toContainText("Incorrect");
 
-    //toPrint
-    // console.log(await CardTitles.nth(0).textContent());
-    // console.log(await CardTitles.first.textContent());
-    //to print all - this will be flaky since playwright does not have wait for this
-    //to over come we can use wait until network idle\
-    await page.waitForLoadState('networkidle');
-    //or we can use
-    //await CardTitles.waitFor(); //- only when single elements so we can use
-    // await CardTitles.first().waitFor();
-    console.log(await CardTitles.allTextContents());
+//     //toPrint
+//     // console.log(await CardTitles.nth(0).textContent());
+//     // console.log(await CardTitles.first.textContent());
+//     //to print all - this will be flaky since playwright does not have wait for this
+//     //to over come we can use wait until network idle\
+//     await page.waitForLoadState('networkidle');
+//     //or we can use
+//     //await CardTitles.waitFor(); //- only when single elements so we can use
+//     // await CardTitles.first().waitFor();
+//     console.log(await CardTitles.allTextContents());
 
-});
+// });
 
-test('UI Controls', async ({page})=>
-    {
-        //elements
-        const UserName = page.locator("#username");
-        const Password = page.locator("[id='password']");
-        const CheckBox = page.locator("[type='checkbox']");
-        const SignInBtn = page.locator("#signInBtn");
-        const DropDown = page.locator("select.form-control");
-        const RadioBtn = page.locator(".checkmark");
-        const PopUp = page.locator("#okayBtn");
-        const CardTitles = page.locator(".card-body a");
-        const BlinkingTxt = page.locator(".blinkingText");
+// test('UI Controls', async ({page})=>
+//     {
+//         //elements
+//         const UserName = page.locator("#username");
+//         const Password = page.locator("[id='password']");
+//         const CheckBox = page.locator("[type='checkbox']");
+//         const SignInBtn = page.locator("#signInBtn");
+//         const DropDown = page.locator("select.form-control");
+//         const RadioBtn = page.locator(".checkmark");
+//         const PopUp = page.locator("#okayBtn");
+//         const CardTitles = page.locator(".card-body a");
+//         const BlinkingTxt = page.locator(".blinkingText");
 
-        //actions
-        await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
-        await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-        await UserName.fill("rahulshettyacademy");
-        await Password.fill("learning");
-        await DropDown.selectOption("consult");
-        //await page.pause(); //To debug
-        await RadioBtn.last().click();
-        expect (RadioBtn.last()).toBeChecked(); //assertion
-        await PopUp.click();
-        await CheckBox.click();
-        expect (CheckBox).toBeChecked();
-        await SignInBtn.click();
-        await expect(page).toHaveTitle("ProtoCommerce");
-        console.log(await CardTitles.allTextContents());
-                //MARCH 16
-        //attribute assertion
-        await expect(BlinkingTxt).toHaveAttribute("Class","blinkingText");
-        await BlinkingTxt.click();
+//         //actions
+//         await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+//         await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+//         await UserName.fill("rahulshettyacademy");
+//         await Password.fill("learning");
+//         await DropDown.selectOption("consult");
+//         //await page.pause(); //To debug
+//         await RadioBtn.last().click();
+//         expect (RadioBtn.last()).toBeChecked(); //assertion
+//         await PopUp.click();
+//         await CheckBox.click();
+//         expect (CheckBox).toBeChecked();
+//         await SignInBtn.click();
+//         await expect(page).toHaveTitle("ProtoCommerce");
+//         console.log(await CardTitles.allTextContents());
+//                 //MARCH 16
+//         //attribute assertion
+//         await expect(BlinkingTxt).toHaveAttribute("Class","blinkingText");
+//         await BlinkingTxt.click();
         
 
-        // await SignInBtn.click();
-        // await expect(page).toHaveTitle("ProtoCommerce");
-        // console.log(await CardTitles.allTextContents());
+//         // await SignInBtn.click();
+//         // await expect(page).toHaveTitle("ProtoCommerce");
+//         // console.log(await CardTitles.allTextContents());
         
         
-    });
+//     });
 
 
-    test.only("Child Windows Handling",async ({browser})=>
-    {   
-        const context = await browser.newContext();
-        const page = await context.newPage();
-        //elements
-        const BlinkingTxt = page.locator(".blinkingText");
-        //const UserName = page.locator("#username");
+//     test("Child Windows Handling",async ({browser})=>
+//     {   
+//         const context = await browser.newContext();
+//         const page = await context.newPage();
+//         //elements
+//         const BlinkingTxt = page.locator(".blinkingText");
+//         //const UserName = page.locator("#username");
 
-        //actions
-        await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+//         //actions
+//         await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
         
-        //page opening action should be executed parallel with listener
-        const [newPage] = await Promise.all(
-            [
-                context.waitForEvent("page"),
-                BlinkingTxt.click(),
-        ]);
+//         //page opening action should be executed parallel with listener
+//         const [newPage] = await Promise.all(
+//             [
+//                 context.waitForEvent("page"),
+//                 BlinkingTxt.click(),
+//         ]);
 
-        const text = await newPage.locator(".red").first().textContent();
-        const arrayTxt = text.split("@");
-        const domain = arrayTxt[1].split(" ");
-        const userId = domain[0].split(".");
-        //console.log(userId[0]);
-        await page.locator("#username").fill(userId[0]);
-        console.log(await page.locator("#username").textContent());      
+//         const text = await newPage.locator(".red").first().textContent();
+//         const arrayTxt = text.split("@");
+//         const domain = arrayTxt[1].split(" ");
+//         const userId = domain[0].split(".");
+//         //console.log(userId[0]);
+//         await page.locator("#username").fill(userId[0]);
+//         console.log(await page.locator("#username").textContent());      
 
-        //mar17 eve 
-    });
+//         //mar17 eve 
+//     });
